4ee94131a3f97b521e048065533bae207b55e139
aqua <aqua.server.tech@gmail.com>
2020-01-13 18:37:39 +0900

    オーディオ再生

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<player
+		v-bind:src="'../assets/rapid_galaxy_stream.mp3'"></player>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlayerComponent from './components/player.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+	player: PlayerComponent
   }
 }
 </script>
---
+++ src/components/player.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="player">
+		<audio preload = "auto" controls="" v-bind:src="src"></audio>
+		<div id="play"
+			@click="play()">再生
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'player',
+  props: {
+	src: String
+  },
+  data: function () {
+	return {audio: new Audio()
+	}
+		},
+  methods: {
+		play: function () {
+			this.audio.src = this.src;
+			this.audio.load();
+			this.audio.addEventListener('canplay', () => {
+				this.audio.play();
+			})
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
